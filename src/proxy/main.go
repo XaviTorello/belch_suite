ff6a4a130c40f8e3a4e3621d4590a3b40c3c1bca
Xavi Torelló <info@xaviertorello.cat>
2018-08-19 17:21:05 +0200

    Prepare the proxy command //WIP

--- src/proxy/main.go
+++ src/proxy/main.go
@@ -1,20 +1,16 @@
-package proxy
+package main
 
 import (
 	"flag"
-
-	"github.com/elazarl/goproxy"
 )
 
 var Defaults = Proxy_config{":8080", false}
 
 func main() {
-	var proxy = goproxy.NewProxyHttpServer()
 	var verbose = flag.Bool("v", Defaults.Verbose, "activate requess logging to stdout")
 	var addr = flag.String("addr", Defaults.Addr, "proxy listen address")
 
 	flag.Parse()
-	proxy.Verbose = *verbose
 
 	var proxy_config = Proxy_config{*addr, *verbose}
 
